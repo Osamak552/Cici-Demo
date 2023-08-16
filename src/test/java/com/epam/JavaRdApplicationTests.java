139f7cfb0f613365b41a72ebd804dc5d702d2f56
MohammadOsama Khan <mohammadosama_khan@epam.com>
2023-08-16 09:04:25 +0530

    added rd-gold project

--- src/test/java/com/epam/JavaRdApplicationTests.java
+++ src/test/java/com/epam/JavaRdApplicationTests.java
@@ -1,13 +1,14 @@
-package com.epam.cicddemo;
+package com.epam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CicdDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+class JavaRdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
