88b7f208eb91a077c9218cdb546dd75ffe9358bb
MohammadOsama Khan <mohammadosama_khan@epam.com>
2023-08-16 12:52:30 +0530

    Add jar

--- src/main/java/com/epam/JavaRdApplication.java
+++ src/main/java/com/epam/JavaRdApplication.java
@@ -2,6 +2,12 @@ package com.epam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//sqp_d47a6ea3f138ca8935d12c2e246f9486f6c90ab1
+//TOKEN
+
+//mvn clean verify sonar:sonar -Dsonar.projectKey=DEMO-TOKEN -Dsonar.projectName='DEMO TOKEN'
+//		-Dsonar.host.url=http://localhost:9000 -Dsonar.token=sqp_d47a6ea3f138ca8935d12c2e246f9486f6c90ab1
 
 @SpringBootApplication
 public class JavaRdApplication {
